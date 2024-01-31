6d3bfcf9438db4e8b16b48933520526ba1a944e7
Daria Yakimenok <dashasim522139@yandex.ru>
2024-01-31 23:03:29 +0700

    first version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+
+// @todo: DOM узлы, место вставки карточек
+
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function addCard(card, deleteCard) {
+    //содержимое шаблона нужно клонировать
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    //добавила название и картинку
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = card.link;
+    cardImage.alt = 'фотография ' + card.name;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function () {
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
+
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+};
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(card => {
+    const cardElement = addCard(card, deleteCard);
+    cardContainer.append(cardElement);
+});
